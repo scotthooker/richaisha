623a1b36bf95878e8b403b88dd0c2fc4f6ec4e12
Scott Hooker <scott.hooker@gmail.com>
2022-05-21 14:12:21 +0100

    Unescape words

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,7 +4,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll }) => {
   return (
     <div className="mt-6 flex items-center justify-between sticky top-0 z-10 bg-white">
         {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <h1 className="font-medium mob:p-2 laptop:p-0">Scott and Jess\'s slightly weird attempt at a thank you... (Entirely Scott\'s idea)</h1>
+      <h1 className="font-medium mob:p-2 laptop:p-0">Scott and Jess's slightly weird attempt at a thank you... (Entirely Scott's idea)</h1>
       <div>
 
       </div>
